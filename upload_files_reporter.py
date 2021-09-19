840c053b4746beedd6bfd4e7b21a2a71418d9c06
mnz0 <mansaaa@gmail.com>
2021-09-20 00:31:57 +0300

    Update upload_files_reporter.py
    add main func
    tune variables
    add counter

--- upload_files_reporter.py
+++ upload_files_reporter.py
@@ -3,11 +3,13 @@ from pathlib import Path
 import datetime
 import os # for os.listdir
 
+upload_folder = Path("D:\Distr")   #Temporart. Path will set in Main func
 
 def get_today_date():
     today = datetime.date.today
     return today().strftime("%Y%m%d")
 
+today_date_str = get_today_date()
 
 def get_folders_list(path):
     folders_list = os.listdir(path)
@@ -15,14 +17,18 @@ def get_folders_list(path):
     
 
 def write_report_file(dirs_list):
+    entries_count = 0
     with open(today_date_str + ".txt", "a+") as f:
         for folder in dirs_list:
-            f.write(folder + "\n")
+            entries_count +=1 
+            f.write(str(entries_count) + ". " +  folder + "\n")
 
+def main(path): 
+    if Path(path).exists:
+        print("Path exist...")
+        folders = get_folders_list(path)
+        write_report_file(folders)
+    else:
+        print("Path is not exitst...")
 
-upload_folder = Path("C:\Program Files")
-today_date_str = get_today_date()
-folders_list = get_folders_list(upload_folder)
-
-
-write_report_file(folders_list)
+main(upload_folder)
