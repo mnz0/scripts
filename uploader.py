4f67d24e5565ce3ea8fb192c064064de6fa1d37f
mnz0 <mansaaa@gmail.com>
2021-09-25 21:03:39 +0300

    add path folders

--- uploader.py
+++ uploader.py
@@ -4,8 +4,10 @@ import shutil
 import datetime
 from pathlib import Path
 
-
-PROJECT_PATH = Path("C:\tmp")
+FOLDERS_PATTERN = ['SRC', 'RENDER','RESULT']
+RESULT_PATTERN = ['DPX','TIF','DAILIES','JPEG']
+PROJECT_PATH = Path("C:\prog\tmp")
+FTP_PROJECT_PATH = Path("C:\prog\tmp\ftp")
 
 def last_version_parse():
     pass
@@ -13,8 +15,9 @@ def last_version_parse():
 def copy_dailies_to_ftp():
     pass
 
-def copy_dpx_to_ftp():
-    pass
+def copy_dpx_to_ftp(src, ftp_dest):
+    Path
+    shutil.copy(src,ftp_dest)
 
 def copy_matte_to_ftp():
     pass
@@ -26,3 +29,4 @@ def get_date():
     today = datetime.date.today()
     return today.strftime("%Y%m%d")
     
+print(RESULT_PATTERN[2])
