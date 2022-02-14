7b4e6fe62234601c7deeebadd02976215efd32a9
mnz0 <mansaaa@gmail.com>
2022-02-15 01:27:26 +0300

    git_crash_01

--- uploader.py
+++ uploader.py
@@ -46,35 +46,7 @@ def get_shot_list():
 
     # print(episode_list)
     print(shot_db)
-=======
-    episode_size = 0
-
-
-    for episode_index, ep_folder in enumerate(folders_list): #get index from list dirs
-
-        if("SC" in ep_folder or "EP" in ep_folder or "S" or "SE" in ep_folder):
-            episode_list.append(PROJECT_PATH.joinpath(ep_folder))
-
-    episode_size = len(episode_list)
-
-    while(episode_size != 0):
-
- 
-        os.chdir(episode_list[episode_size-1])
-        print(Path.cwd())
-        episode_size -= 1
-        scan_shot_list = [shot for shot in os.listdir(cur_cwd)] # generator for light  list shots 
-        glob.glob("..")# up to ep list
-
-    for shot_index,shot in enumerate(episode_list):
-        
-            shot_list.append(os.listdir(shot))
-
-               #TODO Dive into shot and append shot name to global path
-            # os.chdir(prew_cwd)
->>>>>>> b4191ae61ab5c5771f3dcdb817f638aa07d72c7f
     
-
 def last_version_parse(path):
     os.chdir(Path(path))
     upper_version = "v000"
